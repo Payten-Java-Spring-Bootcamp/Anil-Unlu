daecf10a228e7b1c0c11e51376e82194689a4e94
kodluyoruz <info@kodluyoruz.org>
2021-12-26 15:41:12 +0300

    week demo finish

---
+++ demoWeek/src/main/java/com/anilunlu/DemoWeekApplication.java
@@ -0,0 +1,15 @@
+package com.anilunlu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWeekApplication.class, args);
+	}
+	
+
+
+}
---
+++ demoWeek/src/test/java/com/anilunlu/DemoWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anilunlu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
